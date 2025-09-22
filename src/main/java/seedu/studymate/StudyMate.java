55ab7f1173b500dccb7dcdc3a875d416ac23ac84
Rayan-Wong <134691628+Rayan-Wong@users.noreply.github.com>
2025-09-22 16:08:04 +0800

    Fully renamed everything to StudyMate

---
+++ src/main/java/seedu/studymate/StudyMate.java
@@ -0,0 +1,16 @@
+package seedu.studymate;
+
+import java.util.Scanner;
+
+public class StudyMate {
+    /**
+     * Main entry-point for the java.studymate.StudyMate application.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello from\n" + "StudyMate");
+        System.out.println("What is your name?");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
+    }
+}
--- src/test/java/seedu/studymate/StudyMateTest.java
+++ src/test/java/seedu/studymate/StudyMateTest.java
@@ -1,10 +1,10 @@
-package seedu.duke;
+package seedu.studymate;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-class DukeTest {
+class StudyMateTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
