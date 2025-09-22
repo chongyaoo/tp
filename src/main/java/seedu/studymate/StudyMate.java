c0b6f8010f5e8b1a18a9438365bc810cfa8fe295
Rayan-Wong <134691628+Rayan-Wong@users.noreply.github.com>
2025-09-22 16:21:22 +0800

    Fixed tests

--- src/main/java/seedu/studymate/StudyMate.java
+++ src/main/java/seedu/studymate/StudyMate.java
@@ -7,7 +7,7 @@ public class StudyMate {
      * Main entry-point for the java.studymate.StudyMate application.
      */
     public static void main(String[] args) {
-        System.out.println("Hello from\n" + "StudyMate");
+        System.out.println("Hello from StudyMate");
         System.out.println("What is your name?");
 
         Scanner in = new Scanner(System.in);
